471a7469225ca63e5ee7a428dbe4225b069d9f3c
Anthony Lieu <anthonyjunlieu@gmail.com>
2025-05-07 00:43:42 -0400

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import Navbar from './components/Navbar';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Experience from './pages/Experience';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
 
 function App() {
   return (
-    <><Navbar /><Home /><About /><Experience /><Projects /><Contact /></>
+    <><Navbar /></>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,6 @@
-import { useEffect, useState } from 'react';
-import './Navbar.css';
-
 function Navbar() {
-  const [isTransparent, setIsTransparent] = useState(true);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      setIsTransparent(currentScrollY < 100);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
   return (
-    <header className={`navbar ${isTransparent ? 'transparent' : ''}`}>
-      <nav className="navbar-container">
-        <h1 className="navbar-brand">
-          <a href="/">Anthony Lieu</a>
-        </h1>
-        <ul className="navbar-links">
-          <li><a href="#about">About</a></li>
-          <li><a href="#experience">Experience</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-    </header>
+    <p>joe</p>
   );
 }
 
