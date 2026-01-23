6ebf80af5c164580240adcc905599e20b09fb2e4
Anthony Lieu <anthonyjunlieu@gmail.com>
2026-01-22 22:07:22 -0500

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import { useState, useEffect } from "react";
 import About from "./About"
 import Projects from "./Projects"
 import "./App.css";
 
 export default function App() {
-  const [showArrow, setShowArrow] = useState(true);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setShowArrow(false);
-      } else {
-        setShowArrow(true);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
 
   return (
     <div id="app">
